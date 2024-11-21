a67524c78a86d89a768f6424e9fc9ee593a207ed
Moreno Marelli <mormar78@gmail.com>
2024-11-21 18:16:12 +0100

    aggiunto funzione post del backend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 // creo un server di backend
-import express from "express";
+import express, { json } from "express";
 
 const app = express();
+
+app.use(express.json());
 
 app.listen(8800, () => {
   console.log("---- Backend connesso SUCCESSO ----");
@@ -17,7 +19,7 @@ const db = sql.createConnection({
   database: "test",
 });
 
-//funzione diprova per vedere se il back + leggibile
+//funzione diprova per vedere se il backend è leggibile
 /*app.get("/", (req, res) => {
   res.json("Ciao questo è il backend");
 });*/
@@ -31,6 +33,41 @@ app.get("/", (req, res) => {
     if (err) {
       return res.json(err);
     } else {
+      return res.json(data);
+    }
+  });
+});
+
+//CREATE
+//creo la parte di backend che mi permette di inserire un nuovo record nel database
+// all'inizio uso postman che mi permetterá di testare se il backend funziona in questo senso
+
+app.post("/books", (req, res) => {
+  const queryInserisciNuovoLibro =
+    "INSERT INTO test.books (`title`, `description`, `cover`,`price`) VALUES (?);";
+
+  // questi valori li uso per fare una prova con postman
+  //(dentro postman faccio la richiesta senza aver ancora inserito alcun json)
+  /* const values = [
+    "title from backend 1117",
+    "description from backend 11117",
+    "cover from backend 11117",
+    "11117",
+  ];*/
+
+  //  questi valori li inserisco da post man
+  const values = [
+    req.body.title,
+    req.body.description,
+    req.body.cover,
+    req.body.price,
+  ];
+
+  db.query(queryInserisciNuovoLibro, [values], (err, data) => {
+    if (err) {
+      return res.json.err;
+    } else {
+      //return res.json("libro inserito con successo");
       return res.json(data);
     }
   });
